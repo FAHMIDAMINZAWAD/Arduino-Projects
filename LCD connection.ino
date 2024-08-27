f0e98941bcf323b87b1bd74a47b43ce3a037a995
Fahmid Zawad <165585728+FAHMIDAMINZAWAD@users.noreply.github.com>
2024-08-27 20:04:10 +0600

    Add files via upload

---
+++ LCD connection.ino
@@ -0,0 +1,33 @@
+#include<LiquidCrystal_I2C.h>
+
+const int LCD_ADDRESS = 0x27;
+const int LCD_COLUMNS = 16;
+const int LCD_ROWS = 2;
+
+LiquidCrystal_I2C lcd (LCD_ADDRESS, LCD_COLUMNS, LCD_ROWS);
+
+void setup (){
+
+lcd.init ();
+lcd.clear ();
+lcd.backlight();  
+
+lcd.setCursor (5, 0);
+lcd.print("Hello");
+  lcd.setCursor(5,1);
+  lcd.print("World!");
+  
+
+}
+
+  
+
+
+void loop()
+{
+  
+  
+  
+  
+  
+}
